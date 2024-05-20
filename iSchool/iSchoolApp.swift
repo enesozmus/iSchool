cc04699aadb3ffe09bdfda762297f5985a557dcb
enesozmus <ensozmus@gmail.com>
2024-05-20 20:55:58 +0300

    Initial Commit

---
+++ iSchool/iSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSchoolApp.swift
+//  iSchool
+//
+//  Created by enesozmus on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct iSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
