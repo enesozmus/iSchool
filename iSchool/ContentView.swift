2227373828933519f9528ab010deff5cba126f05
enesozmus <ensozmus@gmail.com>
2024-05-20 22:20:39 +0300

    🔨 Listing datas and showing them on the screen and Adding other functionality

--- iSchool/ContentView.swift
+++ iSchool/ContentView.swift
@@ -8,17 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Get a reference to the CoreDataStack from the environment.
+    @EnvironmentObject var coreDataStack : CoreDataStack
+    // Get a reference to the managed object context from the environment.
+    @Environment(\.managedObjectContext) private var viewContext
+    // Get schools
+    @FetchRequest(sortDescriptors: []) var schools: FetchedResults<School>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Section {
+                    List {
+                        ForEach(schools) { school in
+                            NavigationLink {
+                                Text("DetailView")
+                            } label: {
+                                VStack(alignment: .leading) {
+                                    Text(school.name ?? "")
+                                        .foregroundStyle(.white)
+                                        .font(.headline)
+                                        .padding()
+                                        .background(.pink, in: RoundedRectangle(cornerRadius: 12))
+                                }
+                            }
+                        }
+                        .onDelete(perform: deleteSchool)
+                        
+                        Button {
+                            addARandomSchool()
+                        } label: {
+                            Text("Add a random school for test")
+                        }
+                    }
+                    .listStyle(.insetGrouped)
+                }
+            }
+            .navigationTitle("iSchool")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        coreDataStack.databaseSeeding()
+                    } label: {
+                        Text("Database seeding")
+                    }
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        coreDataStack.deleteAll()
+                    } label: {
+                        Text("Remove all")
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    //
+    func addARandomSchool() {
+        let newSchool = School(context: viewContext)
+        newSchool.id = UUID()
+        newSchool.name = "A Random School"
+        newSchool.address = ""
+        newSchool.date = Date()
+        coreDataStack.save()
+    }
+    func deleteSchool(offsets : IndexSet) {
+        offsets.map { schools[$0] }.forEach(viewContext.delete)
+        coreDataStack.save()
     }
 }
 
 #Preview {
     ContentView()
+        .environment(\.managedObjectContext, CoreDataStack.shared.persistentContainer.viewContext)
+        .environmentObject(CoreDataStack.shared)
 }
